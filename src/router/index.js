249c149b4261c334258487b05e061dd0b6d85db4
Crepiks <sayazhan.onlassyn@mail.ru>
2020-04-26 16:44:15 +0600

    fix: delete useless code

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-import store from "../store/index.js";
-import Login from "../components/Login.vue";
-import Secure from "../components/Secure.vue";
-import Register from "../components/Register.vue";
+import store from "@/store/index.js";
+
+import Home from "@/views/Home.vue";
+import Login from "@/views/Login.vue";
+import Secure from "@/views/Secure.vue";
+import Register from "@/views/Register.vue";
 
 Vue.use(VueRouter);
 
@@ -31,15 +32,6 @@ const routes = [
     meta: {
       requiresAuth: true
     }
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
   }
 ];
 
