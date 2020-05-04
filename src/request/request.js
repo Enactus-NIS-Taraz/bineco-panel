d20a21e36452da62e8caf070be2c68cfe8d2ddfb
Crepiks <sayazhan.onlassyn@mail.ru>
2020-05-04 23:49:00 +0600

    feat: show request progress message

---
+++ src/request/request.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { message } from "ant-design-vue";
+
+const key = "request";
+axios.interceptors.request.use(
+  config => {
+    message.loading({ content: "Please wait", key });
+    return config;
+  },
+  err => {
+    message.error({ content: "Error", key });
+    return Promise.reject(err);
+  }
+);
+
+axios.interceptors.response.use(
+  res => {
+    message.success({ content: "Success", key });
+    return res;
+  },
+  err => {
+    message.error({ content: "Error", key });
+    return Promise.reject(err);
+  }
+);
+
+export default axios;
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "@/request/request";
 import config from "@/config/config";
 
 export default {
