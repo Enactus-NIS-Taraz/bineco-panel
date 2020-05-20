8e6bebfdadb3fe8495f473da61e64b19ba7d7bc7
Crepiks <sayazhan.onlassyn@mail.ru>
2020-05-20 10:02:28 +0600

    feat: add yandex map to the Map page

--- src/views/devices/Map.vue
+++ src/views/devices/Map.vue
@@ -1,7 +1,41 @@
 <template>
-  <h1>This is devices map</h1>
+  <yandex-map :settings="settings" :coords="coords" :zoom="zoom" class="map">
+    <ymap-marker markerId="1" marker-type="Placemark" :coords="coords" />
+  </yandex-map>
 </template>
 
-<script></script>
+<script>
+import { yandexMap, ymapMarker } from "vue-yandex-maps";
+import config from "@/config/config";
+import { mapGetters } from "vuex";
 
-<style></style>
+const settings = {
+  apiKey: config.ymapApiKey,
+  lang: "ru_RU",
+  coordorder: "latlong",
+  version: "2.1"
+};
+
+export default {
+  components: {
+    yandexMap,
+    ymapMarker
+  },
+  data() {
+    return {
+      settings,
+      coords: [42.881309040491736, 71.3762154736328],
+      zoom: 12
+    };
+  },
+  computed: {
+    ...mapGetters(["devices"])
+  }
+};
+</script>
+
+<style scoped>
+.map {
+  height: 65vh;
+}
+</style>
