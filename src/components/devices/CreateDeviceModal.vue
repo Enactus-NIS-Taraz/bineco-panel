907b6befcc305d42956ac78c304eea2dac6a4861
Crepiks <sayazhan.onlassyn@mail.ru>
2020-05-24 22:01:13 +0600

    feat: create modal which opens when add device button is pressed

---
+++ src/components/devices/CreateDeviceModal.vue
@@ -0,0 +1,17 @@
+<template>
+  <a-modal :visible="visible" @change="$emit('change', false)"></a-modal>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: "visible",
+    event: "change"
+  },
+  props: {
+    visible: Boolean
+  }
+};
+</script>
+
+<style></style>
