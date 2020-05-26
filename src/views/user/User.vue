62fb1b532e49b19eed6166f7639c6819151e47d2
Crepiks <sayazhan.onlassyn@mail.ru>
2020-05-26 22:38:31 +0600

    feat: add data card for email

--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="main">
+  <div class="user">
     <data-card
+      class="user__data-card"
       title="Your Name"
       description="Please enter your full name, or a display name you are comfortable with."
       defaultValue="Sayazhan Onglassyn"
       footerDescription="Please use 32 characters at maximum."
+    />
+    <data-card
+      class="user__data-card"
+      title="Your Email"
+      description="Please enter the email address you want to use to log in with BinEco Dashboard."
+      defaultValue="test@mail.ru"
+      footerDescription="We will email you to verify the change."
     />
   </div>
 </template>
@@ -19,4 +27,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.user__data-card {
+  margin-bottom: 20px;
+}
+</style>
