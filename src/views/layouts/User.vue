6871743ecf99287118ce85484fce0b0f89886458
Crepiks <sayazhan.onlassyn@mail.ru>
2020-05-26 22:07:24 +0600

    feat: create user layout and index page file

---
+++ src/views/layouts/User.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="user-layout">
+    <nav class="user-layout__nav">This is nav</nav>
+    <router-view class="user-layout__content" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.user-layout {
+  width: 100%;
+  display: flex;
+  box-sizing: border-box;
+}
+
+.user-layout__nav {
+  width: 20%;
+}
+
+.user-layout__content {
+  width: 80%;
+}
+</style>
