1c6fc0d3c8869fc0fd77f9028cf479ac564ddb13
Crepiks <sayazhan.onlassyn@mail.ru>
2020-05-26 22:34:22 +0600

    feat: create user data card

--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="main">
-    <h1>This is profile page</h1>
+    <data-card
+      title="Your Name"
+      description="Please enter your full name, or a display name you are comfortable with."
+      defaultValue="Sayazhan Onglassyn"
+      footerDescription="Please use 32 characters at maximum."
+    />
   </div>
 </template>
 
-<script></script>
+<script>
+import DataCard from "@/components/user/DataCard";
+
+export default {
+  components: {
+    "data-card": DataCard
+  }
+};
+</script>
 
 <style></style>
