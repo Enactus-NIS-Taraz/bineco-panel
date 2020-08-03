c48b1e43d2545238bb68c828ef71e5f198c819bf
Crepiks <sayazhan.onlassyn@mail.ru>
2020-08-03 19:14:42 +0600

    feat: change theme of ant design instance

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import Antd from "ant-design-vue";
-import "ant-design-vue/dist/antd.css";
 
+import Antd from "ant-design-vue";
+import "../node_modules/ant-design-vue/dist/antd.less";
 Vue.use(Antd);
 Vue.config.productionTip = false;
 
---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      less: {
+        lessOptions: {
+          modifyVars: {
+            "primary-color": "#569980",
+            "link-color": "#FA5059",
+            "border-radius-base": "5px"
+          },
+          javascriptEnabled: true
+        }
+      }
+    }
+  }
+};
